4dd90f7f9037b251f47319b1be1b1e3747b54c74
unknown <sonamyangchen12190084>
2020-11-27 10:50:43 +0600

    lab7

---
+++ Lab/lab7/Solution.java
@@ -0,0 +1,196 @@
+import java.util.*;
+
+public class Solution<Key extends Comparable<Key>, Value> {
+    Node root;             // root of BST
+
+    class Node {
+        Key key;           // sorted by key
+        Value val;         // associated data
+        Node left, right;  // left and right subtrees
+        int size;          // number of nodes in subtree
+
+        public Node(Key key, Value val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
+    /**
+     * Initializes an empty symbol table.
+     */
+    public Solution() {
+        root = null;
+        left = null;
+        right = null;
+        size = null; 
+
+    }
+
+    /**
+     * Returns true if this symbol table is empty.
+     * @return {@code true} if this symbol table is empty; {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        if(size==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+       
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return size;
+      
+       
+    }
+
+    // return number of key-value pairs in BST rooted at x
+    private int size(Node x) {
+        
+        
+       
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     *
+     * @param  key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *         {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    // public boolean contains(Key key) {
+       
+    // }
+
+    /**
+     * Returns the value associated with the given key.
+     *
+     * @param  key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *         and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Value get(Key key) {
+        
+    }
+
+    private Value get(Node x, Key key) {
+       
+        
+        
+    }
+
+    /**
+     * Inserts the specified key-value pair into the symbol table, overwriting the old 
+     * value with the new value if the symbol table already contains the specified key.
+     * Deletes the specified key (and its associated value) from this symbol table
+     * if the specified value is {@code null}.
+     *
+     * @param  key the key
+     * @param  val the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(Key key, Value val) {
+        
+       
+    }
+
+    // private Node put(Node x, Key key, Value val) {
+        
+    // }
+
+    /**
+     * Returns the smallest key in the symbol table.
+     *
+     * @return the smallest key in the symbol table
+     * @throws NoSuchElementException if the symbol table is empty
+     */
+    public Key min() {
+       
+    } 
+
+    private Node min(Node x) { 
+         
+    } 
+
+   
+
+    /**
+     * Returns the largest key in the symbol table less than or equal to {@code key}.
+     *
+     * @param  key the key
+     * @return the largest key in the symbol table less than or equal to {@code key}
+     * @throws NoSuchElementException if there is no such key
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Key floor(Key key) {
+        
+    } 
+
+    // private Node floor(Node x, Key key) {
+    //     return null;
+       
+    // } 
+
+    
+    
+
+    /**
+     * Return the key in the symbol table whose rank is {@code k}.
+     * This is the (k+1)st smallest key in the symbol table.
+     *
+     * @param  k the order statistic
+     * @return the key in the symbol table of rank {@code k}
+     * @throws IllegalArgumentException unless {@code k} is between 0 and
+     *        <em>n</em>–1
+     */
+    public Key select(int k) {
+        
+    }
+
+    // Return key of rank k. 
+    private Node select(Node x, int k) {
+        
+         
+    } 
+
+    
+
+    /**
+     * Returns all keys in the symbol table in the given range,
+     * as an {@code Iterable}.
+     *
+     * @param  lo minimum endpoint
+     * @param  hi maximum endpoint
+     * @return all keys in the symbol table between {@code lo} 
+     *         (inclusive) and {@code hi} (inclusive)
+     * @throws IllegalArgumentException if either {@code lo} or {@code hi}
+     *         is {@code null}
+     */
+    public Iterable<Key> keys(Key lo, Key hi) {
+     
+    } 
+
+    private void keys(Node x,Queue<Key> queue, Key lo, Key hi) { 
+        
+    } 
+
+   
+    /* Run the program by giving the approriate command obtained from
+    input files through input.txt files. The output should be displayed
+    exactly like the file output.txt shows it to be.*/
+  
+    public static void main(String[] args) { 
+        
+       
+    }
+}
